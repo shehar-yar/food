81b8c87c3627e9220bfc50ab58d98ea28a8b8618
shehar-yar <mshaheerkhan013@gmail.com>
2024-12-10 13:21:48 +0500

    initial

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+
+  return (
+    <>
+      <h1>Home Page</h1>
+      <Button>Test Button!!!</Button>
+    </>
+  )
+}
+
+export default App
